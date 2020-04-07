7d4639caa15cc098b46547ad5039a57ce77ca9a2
Timothe Faudot <timothe@google.com>
2020-04-07 11:10:22 +0200

    added all necessary output columns as per #10

--- sheet_cleaner/geocoding/test_csv_geocoder.py
+++ sheet_cleaner/geocoding/test_csv_geocoder.py
@@ -10,12 +10,16 @@ class TestCSVGeocoder(unittest.TestCase):
         self.geocoder = csv_geocoder.CSVGeocoder(os.path.join(cur_dir, "geo_admin.tsv"))
 
     def test_found(self):
-        geo = self.geocoder.Geocode("Lyon", "Auvergne-Rhone-Alpes", "France")
-        self.assertAlmostEqual(geo.lat, 45.76)
-        self.assertAlmostEqual(geo.lng, 4.84)
+        geo = self.geocoder.Geocode("Sunac City, Shangcheng, Changchun City", "Jilin", "China")
+        self.assertAlmostEqual(geo.lat, 43.8296097)
+        self.assertAlmostEqual(geo.lng, 125.25924)
         self.assertEqual(geo.geo_resolution, "point")
-        self.assertEqual(geo.country_new, "France")
-        self.assertEqual(geo.admin_id, 1)
+        self.assertEqual(geo.country_new, "China")
+        self.assertEqual(geo.admin_id, 220104)
+        self.assertEqual(geo.location, "Sunac City, Shangcheng")
+        self.assertEqual(geo.admin1, "Jilin")
+        self.assertEqual(geo.admin2, "Changchun City")
+        self.assertEqual(geo.admin3, "Chaoyang District")
 
     def test_not_found(self):
         self.assertIsNone(self.geocoder.Geocode("foo", "bar", "baz"))
